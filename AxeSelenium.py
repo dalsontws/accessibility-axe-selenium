4252063e346b89ac0515ed34a54d376edf6b59b2
cy002 <ychai002@e.ntu.edu.sg>
2019-12-18 12:09:07 +0800

    First commit

--- AxeSelenium.py
+++ AxeSelenium.py
@@ -1,21 +1,19 @@
-from textwrap import wrap
-from matplotlib.figure import Figure
-from matplotlib.backend_bases import key_press_handler
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-import tkinter as tk
-import matplotlib.pyplot as plt
+# from textwrap import wrap
+# from matplotlib.figure import Figure
+# from matplotlib.backend_bases import key_press_handler
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+# import tkinter as tk
+# import matplotlib.pyplot as plt
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from axe_selenium_python import Axe
 from selenium.common import exceptions
-from collections import Counter
-
-from scipy import stats
-
 import time
 import pymsgbox
 import numpy as np
 import pytest
+import json
+from Naked.toolshed.shell import execute_js
 
 
 def make_autopct(values):
@@ -39,6 +37,7 @@ def get_all_links(urls):
         url_list = driver.find_elements_by_tag_name("a")
         for link in url_list:
             fullLink = str(link.get_attribute("href"))
+            print(fullLink)
             if any(substring in fullLink for substring in invalid_links):
                 break
 
@@ -83,40 +82,30 @@ def save_as_json(full_set, full_json):
     count_max = 0
     violations_arr = []
     url_arr = []
-    for link in full_set:
-        print(link)
-        driver.get(link)
+    resu = execute_js('conftest.js')
 
-        axe = Axe(driver)
+    # for i in range(len(full_set)):
+    with open("object.json", "r") as read_file:
+        results = json.load(read_file)
+        print(len(results))
+        for i in range(len(results)):
+            if results[i] is None:
+                continue
+            url = results[i]['url']
 
-        # try options
-        # option = {'rules': {'color-contrast': {'enabled': 'false'},
-        #                     'valid-lang': {'enabled': 'false'}}}
-        # Inject axe-core javascript into page.
-        axe.inject()
-        # Run axe accessibility checks.
-        try:
-            results = axe.run()
-        except:
-            axe = Axe(driver)
-            results = axe.run()
 
-        if (results is None):
-            break
-
-        url = results['url']
         # TODO: Can use dict for violations and url array, using array now for simplicity/pyplot
-        violations_arr = np.append(
-            violations_arr, len(results['violations']))
+            violations_arr = np.append(
+                violations_arr, len(results[i]['violations']))
 
-        url_arr = np.append(url_arr, url)
+            url_arr = np.append(url_arr, url)
 
-        if (len(results['violations']) > count_max):
-            count_max = len(results['violations'])
-            max_url = url
+            if (len(results[i]['violations']) > count_max):
+                count_max = len(results[i]['violations'])
+                max_url = url
 
-        count_passes += len(results['passes'])
-        count_incomplete += len(results['incomplete'])
+            count_passes += len(results[i]['passes'])
+            count_incomplete += len(results[i]['incomplete'])
 
         # print(type(results))
         # print(results.get('violations').count("critical"))
@@ -124,78 +113,77 @@ def save_as_json(full_set, full_json):
         # print('violations: ', count_violations)
         # print('critical violations: ', count_critical)
 
-        full_json[url] = results
-        print("done")
+            full_json[url] = results[i]
+            print("done")
 
-    print(sum(violations_arr))
-    count_arr = [count_incomplete, sum(violations_arr), count_passes]
-    print('Number of violations: ', sum(violations_arr))
+            print(sum(violations_arr))
+            count_arr = [count_incomplete, sum(violations_arr), count_passes]
+            print('Number of violations: ', sum(violations_arr))
     return full_json, violations_arr, url_arr, max_url, count_arr
 
 
-def plot_visualisations(count_arr, violations_arr, url_arr, des_arr, max_url, json_save_path):
-    root = tk.Tk()
-    root.wm_title("title")
-
-    fig = Figure(figsize=(10, 10), dpi=100)
-    labels = 'Passes', 'Violations', 'Incomplete'
-    sizes = count_arr
-    explode = (0, 0.2, 0)
-
-    ax1 = fig.add_subplot(223)
-
-    ax1.pie(sizes, explode=explode, labels=labels, autopct=make_autopct(sizes),
-            textprops={'fontsize': 10}, shadow=True, startangle=90, radius=1.5)
-
-    # max_url = 'https://www.cpf.gov.sg/eSvc/Web/Miscellaneous/Cashier/ECashierHomepage'
-    ax3 = fig.add_subplot(211)
-    table_vals = []
-
-    table_vals.append(['No. of Web Pages', len(url_arr)])
-    table_vals.append(['No. of Violations', str(int(sum(violations_arr)))])
-    table_vals.append(['Most Common Violation', str(stats.mode(des_arr)[0])])
-    table_vals.append(['No. of Passes', str(count_arr[0])])
-    table_vals.append(['Most Violations', max_url])
-    table_vals.append(['Time taken:', "%.1f" % time_taken + "s"])
-    table_vals.append(['Full log:', json_save_path])
-
-    # Draw table
-    the_table = ax3.table(cellText=table_vals,
-                          colWidths=[0.09, 0.3],
-                          rowLabels=None,
-                          colLabels=None,
-                          loc='center')
-    the_table.auto_set_font_size(False)
-    the_table.set_fontsize(10)
-    the_table.scale(3, 3)
-
-    ax3.tick_params(axis='x', which='both', bottom=False,
-                    top=False, labelbottom=False)
-    ax3.tick_params(axis='y', which='both', right=False,
-                    left=False, labelleft=False)
-    for pos in ['right', 'top', 'bottom', 'left']:
-        ax3.spines[pos].set_visible(False)
-
-    j = 1
-    labels = []
-    for l in url_arr:
-        labels.append(j)
-        j = j+1
-    violations = violations_arr
-
-    ax2 = fig.add_subplot(224)
-
-    ax2.bar(labels, violations, align='center', alpha=0.5, tick_label=labels)
-
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.draw()
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-    toolbar = NavigationToolbar2Tk(canvas, root)
-    toolbar.update()
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-    tk.mainloop()
+# def plot_visualisations(count_arr, violations_arr, url_arr, max_url, json_save_path):
+#     root = tk.Tk()
+#     root.wm_title("title")
+#
+#     fig = Figure(figsize=(10, 10), dpi=100)
+#     labels = 'Passes', 'Violations', 'Incomplete'
+#     sizes = count_arr
+#     explode = (0, 0.2, 0)
+#
+#     ax1 = fig.add_subplot(223)
+#
+#     ax1.pie(sizes, explode=explode, labels=labels, autopct=make_autopct(sizes),
+#             textprops={'fontsize': 10}, shadow=True, startangle=90, radius=1.5)
+#
+#     # max_url = 'https://www.cpf.gov.sg/eSvc/Web/Miscellaneous/Cashier/ECashierHomepage'
+#     ax3 = fig.add_subplot(211)
+#     table_vals = []
+#
+#     table_vals.append(['No. of Web Pages', len(url_arr)])
+#     table_vals.append(['No. of Violations', str(int(sum(violations_arr)))])
+#     table_vals.append(['No. of Passes', str(count_arr[0])])
+#     table_vals.append(['Most Violations', max_url])
+#     table_vals.append(['Time taken:', "%.1f" % time_taken + "s"])
+#     table_vals.append(['Full log:', json_save_path])
+#
+#     # Draw table
+#     the_table = ax3.table(cellText=table_vals,
+#                           colWidths=[0.07, 0.3],
+#                           rowLabels=None,
+#                           colLabels=None,
+#                           loc='center')
+#     the_table.auto_set_font_size(False)
+#     the_table.set_fontsize(10)
+#     the_table.scale(3, 3)
+#
+#     ax3.tick_params(axis='x', which='both', bottom=False,
+#                     top=False, labelbottom=False)
+#     ax3.tick_params(axis='y', which='both', right=False,
+#                     left=False, labelleft=False)
+#     for pos in ['right', 'top', 'bottom', 'left']:
+#         ax3.spines[pos].set_visible(False)
+#
+#     j = 1
+#     labels = []
+#     for l in url_arr:
+#         labels.append(j)
+#         j = j+1
+#     violations = violations_arr
+#
+#     ax2 = fig.add_subplot(224)
+#
+#     ax2.bar(labels, violations, align='center', alpha=0.5, tick_label=labels)
+#
+#     canvas = FigureCanvasTkAgg(fig, master=root)
+#     canvas.draw()
+#     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+#
+#     toolbar = NavigationToolbar2Tk(canvas, root)
+#     toolbar.update()
+#     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+#
+#     tk.mainloop()
 
 
 start_time = time.time()
@@ -213,11 +201,11 @@ driver.maximize_window()
 # driver = webdriver.Ie(capabilities=cap)
 # -------- Internet Explorer -------- #
 
-# main_url = "https://www.cpf.gov.sg/members"
-main_url = "https://www.hdb.gov.sg/cs/infoweb/homepage"
+main_url = "https://www.cpf.gov.sg/members"
+# main_url = "https://eservices.healthhub.sg/PersonalHealth"
 
 # -------- Add base URLs -------- #
-urls = {"https://www.hdb.gov.sg/cs/infoweb/homepage"}
+urls = {"https://eservices.healthhub.sg/PersonalHealth"}
 # "https://www.cpf.gov.sg/Members/Schemes"}
 # -------- Add base URLs -------- #
 
@@ -234,24 +222,24 @@ full_set = get_all_links(urls)
 
 full_set = remove_invalid(full_set)
 
+res = dict.fromkeys(full_set, 0)
+
+with open('data.json', 'w') as outfile:
+    json.dump(res, outfile)
+
+
 full_json, violations_arr, url_arr, max_url, count_arr = save_as_json(
     full_set, full_json)
 
-json_save_path = './data/trial_test.json'
+json_save_path = './data/cpf_test6.json'
 axe.write_results(full_json, json_save_path)
-
-des_arr = []
-for items in full_json.values():
-    # print(items['violations'])
-    for item in items['violations']:
-        des_arr.append(item['description'])
 
 driver.close()
 driver.quit()
 time_taken = (time.time() - start_time)
 
-plot_visualisations(count_arr, violations_arr, url_arr, des_arr,
-                    max_url, json_save_path)
+# plot_visualisations(count_arr, violations_arr, url_arr,
+#                     max_url, json_save_path)
 
 
 print("Test Completed")
