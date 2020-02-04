ca0b54d550c9763902bd22bbc48562b9b211dd42
Dalson <43963814+dalsontws@users.noreply.github.com>
2020-02-04 16:11:11 +0800

    Development branch merge (#14)
    * Merge CSV
    
    * Merge CSV
    
    * add CSV command
    
    * Visualisation Panel
    
    * Added features to dashboard
    
    * Input URL from CLI
    
    * Edit trial.js
    
    * Modified mac-run.sh
    
    * File Cleanup
    
    * Cleanup
    
    * Merge test
    
    * merge
    
    * remove conflict headers
    
    Co-authored-by: cy002 <42965395+cy002@users.noreply.github.com>

--- js-axe-selenium/trial.js
+++ js-axe-selenium/trial.js
@@ -1,3 +1,119 @@
+var AxeReports = require('axe-reports')
+var AxeBuilder = require("axe-webdriverjs");
+var WebDriver = require("selenium-webdriver");
+
+var driver = new WebDriver.Builder().forBrowser("chrome").build();
+const fs = require("fs");
+var By = WebDriver.By
+
+var dict = []
+var array = []
+var urls = []
+
+  driver.get(process.env.WEBSITE)
+
+  var promise = require('selenium-webdriver').promise;
+  var links = driver.findElements(By.tagName('a'))
+  links.then(function (elements) {
+    var pendingHref = elements.map(function (elem) {
+        return elem.getAttribute('href');
+    });
+
+    promise.all(pendingHref).then(function (allHref) {
+        var urls = allHref.filter(function (el){
+          return el != null;
+        })
+        var url = urls.filter(function (ele){
+          return ele != 'javascript:;';
+        })
+
+        var url1 = url.filter(function (elem){
+          return elem != '';
+        })
+        var url2 = url1.filter(function (elem){
+              return elem.includes(process.env.WEBSITE);})
+
+        let unique = [...new Set(url2)];
+
+
+//         console.log(unique);
+        
+        
+for (var i=0; i<unique.length;i++){
+    
+    driver.get(unique[i]).then(function(){
+
+    const results = new AxeBuilder(driver)
+//    .configure(config)
+    .analyze().then(function(results){
+    dict.push(results);
+    JSONReport(dict);
+    AxeReports.createCsvReport(results)
+    })
+    })
+
+  }
+
+  driver.close()
+  })
+
+    });
+
+
+
+  const config = {
+  rules: [
+    {
+      id: "fake-rule",
+      selector: "a",
+      enabled: true,
+      tags: ["custom"],
+      all: [],
+      any: ["fake-rule"],
+      none: [],
+      metadata: {
+        description:
+          "This is a rule used for testing the bok choy integration.",
+        help: "I can't help",
+        helpUrl: "There isn't a help url for this!"
+      }
+    }
+  ],
+  checks: [
+    {
+      id: "fake-rule",
+      metadata: {
+        impact: "serious",
+        messages: {
+          pass: "Element has Bok Choy",
+          fail: "Element has no Bok Choy",
+          incomplete: {
+            BokChoy: "Bok Choy could not be seen"
+          }
+        }
+      },
+
+      evaluate: function(node, options) {
+        var href = node.findElements("href");
+
+        if (href === null || href === "" || href === "#") {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+  ]
+};
+
+
+JSONReport = function(dict) {
+  fs.writeFile("./Report.json", JSON.stringify(dict, null, 4), err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
 const AxeReports = require('axe-reports');
 const AxeBuilder = require('axe-webdriverjs');
 const WebDriver = require('selenium-webdriver');
